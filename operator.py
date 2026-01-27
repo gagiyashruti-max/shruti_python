69b5dc36ab29bf69f1b36d2b78d541291fa5f367
gagiyashruti-max <gagiyashruti@gmail.com>
2026-01-27 08:30:36 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,57 @@
+# 1. arithmetic
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+# 2. assignment
+
+x=10
+print(x)
+
+# 3.relational 
+
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+# 4. logical
+
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+# boolean 
+
+is_pass=True
+print(is_pass)
+
+# 6 BITWISE
+
+a=5
+b=3
+print(a&b)
+print(a/b)
+
+# 7. membership
+
+numbers=[1,2,3,4]
+print(2 in numbers)
+print(5 not in numbers)
+
+# 8. identity
+
+x=10
+y-10
+print(x is y)
+print(x is not y)
+
+# 9. unary
+
+a=5
+print (-a)
