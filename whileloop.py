53db05f8455251855f1efb2ec12061fc60cf402f
gagiyashruti-max <gagiyashruti@gmail.com>
2026-02-24 09:13:27 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,25 @@
+# 1. 1 to 10
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+ 
+ 
+  #2  sum
+
+    n=int(input("enter n:"))
+    i=1
+    s=0
+    while i<=n:
+        s=s+i
+        i=i+1
+        print("sum=",s)  
+
+  #3 table of number
+
+    num=int(input("enter number:"))
+    i=1
+    while i<=10:
+        print(num,"x",i,"=",num*i)
+        i=i+1
+
